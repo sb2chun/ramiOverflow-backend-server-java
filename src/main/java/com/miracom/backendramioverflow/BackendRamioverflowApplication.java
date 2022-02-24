512e47796b8c2cee3785fe7c033f31649a5e56d0
sb2ch <sb2chun@gmail.com>
2022-02-24 15:09:58 +0900

    Init: 최초 프로젝트 생성
    폴더 구조 생성
     Posts
      ㄴcontroller/service/repository/entity/dto

---
+++ src/main/java/com/miracom/backendramioverflow/BackendRamioverflowApplication.java
@@ -0,0 +1,13 @@
+package com.miracom.backendramioverflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendRamioverflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendRamioverflowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/miracom/backendramioverflow/BackendRamioverflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miracom.backendramioverflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendRamioverflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
